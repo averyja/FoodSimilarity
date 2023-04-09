680f8aff794502e4fe6e54a43b024084e3fd765f
averyja <jason.aaron.avery@gmail.com>
2023-04-09 14:07:48 -0400

    Update 1

---
+++ FLS_Paper_Analyses.R
@@ -0,0 +1,258 @@
+## Analyses and Figure prep for Avery et al., 2013 Food Similarity Paper
+setwd("~/Documents/GitHub/FoodSimilarity/")
+source("source_me.R")
+
+task = "food_loc"; 
+out_dir = "DATA";
+
+## ========================================================================================== ##
+## ========================================================================================== ##
+## Network Graph of Food ROIs
+## ========================================================================================== ##
+## read in food mat data, if starting from scratch
+food_roi_mat = read.csv(sprintf("%s/food_roi_mat.csv",out_dir))
+food_roi_corr_mat = read.csv(sprintf("%s/food_roi_corr_mat.csv",out_dir))
+
+## ========================================================================================== ##
+## Test for outliers in ROI
+corr_dist_mat = 1 - food_roi_corr_mat
+euc_dist_mat = euc.dist(food_roi_mat)
+## ========================================================================================== ##
+## Calculate Mahalanobis distance for of each ROI from mean of distribution
+
+oas_cov = (CovEst.2010OAS(t(food_roi_mat)))$S
+roi_mahal_dist = sapply(1:length(rois), function(x) mahalanobis(food_roi_mat[,x],rowMeans(food_roi_mat[,-x]),oas_cov))
+names(roi_mahal_dist) = rois
+# V1 has greatest MD of all ROIs
+# Dorsal_ACC  Left_Amyg   Left_dMI   Left_IFG   Left_MFG   Left_OFC   Left_vAI    Left_VS    Pre_SMA Right_Amyg 
+# 19.40317   19.63901   19.69311   19.29921   19.75567   19.36727   19.85763   19.78370   19.56824   19.77713 
+# Right_IFG  Right_IPS  Right_MFG   Right_MI  Right_OFC  Right_PCG         V1 
+# 19.31467   19.51503   19.46706   19.22710   19.86877   19.89786   20.16307 
+scale(roi_mahal_dist) # V1 = 2.09 SD greater than mean
+## V1 is clear outlier
+scale(rowMeans(corr.dist(food_roi_mat))) # V1 = 1.94 SD greater than mean
+scale(rowMeans(euc.dist(food_roi_mat))) # V1 = 2.47 SD greater than mean
+scale(rowMeans(mahal.dist(food_roi_mat))) # V1 = 2.09 SD greater than mean
+## ========================================================================================== ##
+## For following analyses, V1 ROI is removed
+food_roi_corr_mat = cor(food_roi_mat[,1:16])
+
+## ========================================================================================== ##
+# Figure 2C: Make ROI heatmap
+png(sprintf("%s/Food_ROI_heatmap.png",out_dir),width=6,height=6,units="in",res=300)
+  heatmap(as.matrix(food_roi_corr_mat),labCol = NA,cexRow=1)
+dev.off()
+
+## ========================================================================================== ##
+# Figure 2D: Make network graph
+## ========================================================================================== ##
+df = food_roi_corr_mat; diag(df) = 0; df[lower.tri(df)] = 0
+df = melt(as.matrix(df), na.rm = T, value.name = "weight" )
+
+net <- graph.data.frame(df, directed = FALSE)
+net = delete_edges(net, which(E(net)$weight == 0))
+
+V(net)$"label" = rownames(food_roi_corr_mat)
+V(net)$"strength" = rowSums(food_roi_corr_mat, na.rm=TRUE)
+
+comms <- cluster_optimal(net, weights = E(net)$weight) # find optimal network clusters
+V(net)$comms = membership(comms); # write clusters back to graph
+
+roi_color = V(net)$comms
+
+vertex_colors = r_colors[roi_color]
+
+find_edge_color = function(net,x){
+  edge_color = "black"
+  head_color = r_colors[V(net)$comms[head_of(net,x)]]; head_rgb = col2rgb(head_color)
+  tail_color = r_colors[V(net)$comms[tail_of(net,x)]]; tail_rgb = col2rgb(tail_color)
+  mean_color = rgb(t((head_rgb+tail_rgb)/2)/256)
+  edge_color = ifelse(head_color == tail_color, tail_color, mean_color)
+}
+
+edge_colors = sapply(1:length(E(net)), function(x) find_edge_color(net,x))
+
+labels = V(net)$label
+
+png(sprintf("%s/Food_network_graph.png",out_dir),width=8,height=8,units="in",res=300)
+plot.igraph(net,
+            # margin=-.4,
+            #+++++ Vertex
+            vertex.color=vertex_colors,
+            vertex.frame.color = "black",
+            vertex.size=V(net)$strength*3,
+            #+++++ Vertex Label
+            vertex.label=labels,
+            vertex.label.color="Black",
+            vertex.label.family="Helvetica",
+            vertex.label.dist=0,
+            vertex.label.font=2,
+            vertex.label.cex=1, #.5
+            vertex.label.degree=0,
+            #+++++ Edge
+            edge.color=edge_colors,
+            edge.width=(E(net)$weight),
+            edge.curved=0.45,
+            layout= layout_nicely)
+dev.off()
+## ========================================================================================== ##
+## ========================================================================================== ##
+## Do analyses of category and PC1 data
+# Create a data table for all your RSA outputs
+# Loop through subjects then ROIs
+working_dir = "rsa_files_2022-10-30"
+subj_data = list()
+
+for (roi in rois){
+  # make large list for all subj data
+  roi_mat = sapply(subjects, function(x) ut(1-cor(read.csv(sprintf("%s/%s.%s.%s.food_mat.csv",working_dir,x,task,roi)))[,foods]))
+  PC1_dat = apply(roi_mat,2, function(x) fisherz(cor(x,PC1_vec, method = "spearman", use = "complete")))
+  PC2_dat = apply(roi_mat,2, function(x) fisherz(cor(x,PC2_vec, method = "spearman", use = "complete")))
+  cluster_dat = apply(roi_mat,2, function(x) fisherz(cor(x,cluster_vec, method = "spearman", use = "complete")))
+  category_dat = apply(roi_mat,2, function(x) fisherz(cor(x,category_vec, method = "spearman", use = "complete")))
+  
+  subj_data[[roi]] = cbind(subjects,roi,PC1_dat,PC2_dat,cluster_dat,category_dat)
+}
+subj_data = data.frame(do.call("rbind", subj_data)) 
+names(subj_data) = c("subj","roi","PC1","PC2","cluster","category")
+
+for (x in 3:ncol(subj_data)){
+  subj_data[,x]=as.double(as.character(subj_data[,x]));
+}
+subj_data$diff = subj_data[,"cluster"] - subj_data[,"category"]
+
+subj_data$roi = factor(subj_data$roi, levels = rois)
+
+subj_data$network = NA
+subj_data[subj_data$roi %in% hi_road,"network"] = "Prefrontal"
+subj_data[subj_data$roi %in% lo_road,"network"] = "Limbic"
+
+subj_data$network = factor(subj_data$network, levels = c("Prefrontal","Limbic"))
+
+write.csv(subj_data, file = sprintf("%s/RSA_subject_data_%s_zscores.csv",out_dir,task), quote = TRUE, eol = "\n", na = "NA", row.names = TRUE)
+## ========================================================================================== ##
+subj_data = read.csv(sprintf("%s/RSA_subject_data_%s_zscores.csv",out_dir,task),row.names = 1,check.names = F)
+
+grouping_data = melt(subj_data[,c("subj","roi","network","cluster","category")])
+grouping_data$network = factor(grouping_data$network, levels = c("Prefrontal","Limbic"))
+
+D = summary_stats(grouping_data,c('network','variable'),"value")
+
+summary(aov(value ~ variable*network+Error((subj)) , data = grouping_data))
+
+gtests = paired_ttests("variable",grouping_data,"value")
+
+t.test(subj_data$cluster,subj_data$category,paired = T)
+# data:  subj_data$cluster and subj_data$category
+# t = 4.3728, df = 1219, p-value = 1.331e-05
+# alternative hypothesis: true difference in means is not equal to 0
+# 95 percent confidence interval:
+#   0.002887704 0.007587552
+# sample estimates:
+#   mean of the differences 
+# 0.005237628
+t.test(diff ~ network, data = subj_data)
+# data:  diff by network
+# t = 3.0931, df = 855.79, p-value = 0.002045
+# alternative hypothesis: true difference in means is not equal to 0
+# 95 percent confidence interval:
+#   0.003114433 0.013930119
+# sample estimates:
+#   mean in group Prefrontal     mean in group Limbic 
+# 0.011159458              0.002637181 
+
+## ========================================================================================== ##
+## Figure 3D: Compare cluster and category models in Both networks
+## ========================================================================================== ##
+plotting_data = bar_plotting(grouping_data,c('network','variable'),'value')
+myplot = plotting_data$myplot
+D = plotting_data$df
+D$p.adj=p.adjust(D$p.val,method="fdr",n=length((D$p.val)/length(levels(D$variable))))
+
+plot(myplot)
+text_size = 8
+bar_plot = myplot + 
+  annotate("text", label = "", size = text_size, x = c(1:2), y = c(rep((0.03),2))) +
+  geom_signif(comparisons=list(c("cluster","category")), test = "t.test",
+              test.args=list(alternative = "greater", var.equal = TRUE, paired=TRUE),
+              map_signif_level=TRUE,
+              y_position = c(0.025), tip_length = 0, vjust=-0.3, textsize=3) +
+  theme(axis.title.y = element_blank()) + 
+  theme(strip.text = element_text(hjust = 0.5, color="black", size=24, face="bold"))+
+  theme(axis.text.y = element_text(hjust = 0.5, color="black", size=10, face="bold"))+
+  theme(axis.title.x = element_blank()) + theme(axis.text.x = element_blank()) +
+  theme(panel.border = element_rect(colour = "black", fill=NA, size=1), panel.spacing.x = unit(0,"line")) +
+  theme(plot.title = element_text(hjust = 0.5, color="black", size=20, face="bold.italic")) +
+  labs(fill = "Model") + theme(legend.position = "none")
+
+plot(bar_plot)
+ggsave(sprintf("%s/RSA_model_comparison.png",out_dir), width = 4, height = 3,dpi=300)
+## ========================================================================================== ##
+## Figure 3E. Compare diff b/t cluster and category b/t networks
+## ========================================================================================== ##
+subj_data$network = factor(subj_data$network, levels = c("Prefrontal","Limbic"))
+
+plotting_data = bar_plotting(subj_data,"network","diff")
+myplot = plotting_data$myplot
+D = plotting_data$df
+D$p.adj=p.adjust(D$p.val,method="fdr",n=length((D$p.val)/length(levels(D$variable))))
+
+plot(myplot)
+text_size = 8
+bar_plot = myplot + 
+  annotate("text", label = "", size = 3, x = c(1:2), y = c(rep((0.03),2))) +
+  geom_signif(comparisons=list(c("Prefrontal","Limbic")), test = "t.test",
+              test.args=list(alternative = "greater", var.equal = TRUE, paired=FALSE),
+              map_signif_level=TRUE,
+              y_position = c(0.02), tip_length = 0, vjust=-0.3, textsize=3) +
+  theme(axis.title.y = element_blank()) +
+  theme(panel.border = element_rect(colour = "black", fill=NA, size=1)) +
+  theme(axis.title.x = element_blank()) + 
+  theme(axis.text.y = element_text(hjust = 0.5, color="black", size=10, face="bold"))+
+  scale_fill_manual(values=c(r_colors[2],r_colors[1],"green")) +
+  theme(plot.title = element_text(hjust = 0.5, color="black", size=20, face="bold.italic")) +
+  theme(legend.position = "none")
+
+plot(bar_plot)
+ggsave(sprintf("%s/RSA_network_comparison_model_fits.png",out_dir), width = 4, height = 3,dpi=300)
+## ========================================================================================== ##
+## Figure 3F: Compare Both PC models across both networks
+## ========================================================================================== ##
+pc_data = melt(subj_data[,c("subj","roi","network","PC1","PC2")])
+pc_data$network = factor(pc_data$network, levels = c("Prefrontal","Limbic"))
+
+plotting_data = bar_plotting(pc_data,c('variable','network'),'value')
+myplot = plotting_data$myplot
+D = plotting_data$df
+D$p.adj=p.adjust(D$p.val,method="fdr",n=length((D$p.val)/length(levels(D$variable))))
+
+plot(myplot)
+
+text_size = 8
+bar_plot = myplot + 
+  annotate("text", label = "", size = 3, x = c(1:2), y = c(rep((0.03),2))) +
+  geom_signif(comparisons=list(c("Prefrontal","Limbic")), test = "t.test",
+              test.args=list(alternative = "greater", var.equal = TRUE, paired=FALSE),
+              map_signif_level=TRUE,
+              y_position = c(0.025), tip_length = 0, vjust=-0.3, textsize=3) +
+  theme(axis.title.y = element_blank()) +
+  theme(strip.text = element_text(hjust = 0.5, color="black", size=24, face="bold"))+
+  theme(panel.border = element_rect(colour = "black", fill=NA, size=1), panel.spacing.x = unit(0,"line")) +
+  theme(axis.title.x = element_blank()) + 
+  theme(axis.text.y = element_text(hjust = 0.5, color="black", size=10, face="bold"))+
+  scale_fill_manual(values=c(r_colors[2],r_colors[1],"green")) +
+  theme(plot.title = element_text(hjust = 0.5, color="black", size=20, face="bold.italic")) +
+  theme(legend.position = "none")
+
+plot(bar_plot)
+ggsave(sprintf("%s/RSA_network_comparison_2PCS.png",out_dir), width = 4, height = 3,dpi=300)
+
+t.test(PC1 ~ network, data = subj_data)
+# t = 3.1474, df = 813.4, p-value = 0.001707
+# alternative hypothesis: true difference in means is not equal to 0
+# 95 percent confidence interval:
+#   0.004065009 0.017537684
+# sample estimates:
+#   mean in group Prefrontal     mean in group Limbic 
+# 0.015335950              0.004534603 
+
---
+++ source_me.R
@@ -0,0 +1,180 @@
+## A source file for food_loc analyses that contains some variables and helper functions
+# Attach Packages
+library(plyr)
+library(tidyverse)    # data manipulation and visualization
+library(ggsignif)
+library(igraph)
+library(reshape2)
+library(CovTools)
+library(RColorBrewer)
+
+# you may not need these
+library(psych)
+library(sys)
+library(R.utils)
+library(cluster)
+library(factoextra)
+library(FactoMineR)
+library(NbClust)
+
+setwd("~/Documents/GitHub/FoodSimilarity/")
+task = "food_loc"; 
+out_dir = "DATA";
+
+## ========================================================================================== ##
+## Convenience Functions =====
+## ========================================================================================== ##
+# Annotate p-values with significance labels
+sig_label = function(p){
+  ifelse(p>0.05,"NS",ifelse(p<0.001,"***",ifelse(p<0.01,"**","*")))
+}
+## ========================================================================================== ##
+## Function to change the range of an input vector or matrix to between 0..1
+rescale <- function(x, ...){(x - min(x, ...)) / (max(x, ...) - min(x, ...))}
+## ========================================================================================== ##
+## Make a vector from the upper or lower triangle of a matrix
+ut <- function(x){x[upper.tri(x)]}
+## ========================================================================================== ##
+lt <- function(x){x[lower.tri(x)]}
+## ========================================================================================== ##
+graph_plot = function(graph,food_color){
+  #  vertex_colors = sapply(1:length(V(graph)), function(x) color_community[V(graph)$comms[x]])
+  vertex_colors = color_community[food_color]
+  edge_colors = sapply(1:length(E(graph)), function(x) color_community[V(graph)$comms[tail_of(graph,x)]])
+  if(length(V(graph)$label)==0){V(graph)$label=V(graph)$food}
+  labels = V(graph)$label
+  plot.igraph(graph,
+              # margin=-.4,
+              #+++++ Vertex
+              vertex.color=vertex_colors,
+              vertex.frame.color = "white",
+              vertex.size=V(graph)$strength*2,
+              #+++++ Vertex Label
+              vertex.label=labels,
+              vertex.label.color="Black",
+              vertex.label.family="Arial",
+              vertex.label.dist=0,
+              vertex.label.font=3,
+              vertex.label.cex=1, #.5
+              vertex.label.degree=0,
+              #+++++ Edge
+              edge.color=edge_colors,
+              edge.width=(E(graph)$weight),
+              edge.curved=0.45,
+              layout= layout_nicely)
+  #return(plotted_graph)
+}
+## ========================================================================================== ##
+# Aggregate data to get summary stats table
+summary_stats = function(dframe,factor,index){
+  dframe = dframe[,c(factor,index)]; names(dframe) = c(factor,"idx")
+  stats_table = ddply(dframe, c(factor), summarise, N = length(idx[!is.na(idx)]), mean = mean(idx, na.rm = T),
+                      sd = sd(idx, na.rm = T), se = sd/sqrt(N), max_height = mean + se, T = mean/se, 
+                      p.val = round(pt(-abs(T),df=N-1), digits = 3), label = sig_label(p.val))
+  return(stats_table)
+}
+## ========================================================================================== ##
+## New bar plotting function, using settings that I make use of a lot.
+## Ideally, this table will have been melted to long format first, so it has only one numeric value for plotting
+bar_plotting <- function(dframe,factor,index){
+  # First, format input table 
+  value = index
+  variable = ifelse(length(factor) > 1,factor[2],factor[1]) #the name of the variable in your df, whether roi,task, or other factor
+  sprintf("Setting variable as %s, value as %s",variable,value) #print out result for error checking
+  
+  df = summary_stats(dframe,factor,value) #create the data table using function above
+  
+  # set some df variables used for plotting
+  nval = ifelse(length(factor) > 1,length(levels(df[,2])),length(levels(df[,1]))) #if present, number of levels of 2nd factor
+  xval = ifelse(length(factor) > 1,length(levels(df[,1])),1) #if present, number of levels of 2nd factor
+  # some extra df variables used for plotting
+  df[,value] = 0
+  df$x = rep(c(1:nval),xval)
+  
+  # set these variables as parameters for ggplot
+  start_height=max(df$max_height); bar_size = 0; text_size = 3; delta_y = start_height/10; ypos = start_height + 2*delta_y; 
+  
+  myplot <- ggplot(dframe, aes_string(variable, value, fill=variable)) +
+    stat_summary(geom = "bar", fun = mean, position = "dodge", colour="black") +
+    stat_summary(geom = "errorbar", fun.data = mean_se, position = "dodge", width = 0.2) +
+    geom_hline(yintercept=0) +
+    geom_text(y = ypos, aes(label = label), size = text_size, data = df, vjust = "inward") + 
+    theme(axis.text.x = element_text(size = 8,angle=45, hjust=1, face="bold")) +
+    annotate("text", label = "", size = text_size, x = c(1:nval), y = c(rep((ypos+delta_y),nval))) +
+    labs(fill = variable)
+  
+  if (length(factor) > 1){
+    myplot = myplot + facet_wrap(as.formula(paste("~", factor[1]))) + theme(strip.text.x = element_text(size = 8, colour = "black")) 
+  } 
+  
+  # set names and output df and plot
+  output = setNames(list(df,myplot),c("df","myplot"))
+  return(output)
+}
+
+## ========================================================================================== ##
+## Multivariate Distance Metrics =====
+## ========================================================================================== ##
+## ========================================================================================== ##
+## Function for calculating an RDM using regular mahalanobis distance
+mahal.dist = function(your_data,your_labels=colnames(your_data)){
+  X = array(0,c(length(your_labels),length(your_labels)),dimnames = list(your_labels,your_labels)) #make array for all stims
+  your_data = your_data[,your_labels]
+  oas_cov = (CovEst.2010OAS(t(your_data)))$S
+  for (i in 1:(length(your_labels)-1)){
+    x = your_data[,i]
+    for (j in (i+1):length(your_labels)){
+      y = your_data[,j]
+      X[i,j] = mahalanobis(x, y, oas_cov)
+    }
+  }
+  return(X+t(X))
+}
+## ========================================================================================== ##
+## Correlation distance function, implemented in order to match format of other distance functions
+corr.dist <- function(your_data,your_labels=colnames(your_data)){
+  your_data = your_data[,your_labels]
+  X = 1 - cor(your_data, method = "pearson")
+  row.names(X) = your_labels; colnames(X) = your_labels
+  return(X)
+}
+## ========================================================================================== #### simplified function for euclidean distance. Makes function form similar to cor or cos.dist 
+## input and output
+euc.dist = function(your_data,your_labels=colnames(your_data)){
+  your_data = your_data[,your_labels]
+  X = as.matrix(dist(t(your_data), diag = T, upper = T))
+  row.names(X) = your_labels; colnames(X) = your_labels
+  return(X)
+}
+
+## ========================================================================================== ##
+## Useful variables =====
+## ========================================================================================== ##
+r_colors = c(brewer.pal(9,"Reds")[6],"gold",brewer.pal(9,"Blues")[5],brewer.pal(9,"Oranges")[5],brewer.pal(9,"Purples")[5],brewer.pal(9,"Greens")[6])
+r_adj_colors = c(brewer.pal(9,"Reds")[8],"goldenrod4",brewer.pal(9,"Blues")[8],brewer.pal(9,"Oranges")[7],brewer.pal(9,"Purples")[8],brewer.pal(9,"Greens")[8])
+## ========================================================================================== ##
+food_frame = read.csv("DATA/FLS_foods.csv", row.names = 1)
+food_vars = read.csv("DATA/food_pics_data.csv", row.names = 1)
+pval_mat = read.csv("DATA/food_similarity_matrix.csv", row.names = 1)
+foods = row.names(food_vars)
+categories = c("HFLS","HFHS","LFLS","LFHS")
+clusters = c("veggies","fats","fruits","starches","sweets") #assign names to food clusters 
+## ========================================================================================== ##
+## ========================================================================================== ##
+lo_road = c("Dorsal_ACC","Left_Amyg","Left_dMI","Left_vAI","Left_OFC","Left_VS","Right_MI","Right_OFC","Right_Amyg")
+hi_road = c("Left_IFG","Left_MFG","Pre_SMA","Right_IFG","Right_IPS","Right_MFG","Right_PCG")
+visual = "V1"
+roi_order = c(hi_road,lo_road)
+## ========================================================================================== ##
+## Flattened upper triangle vectors used for RSA Analyses
+PC1_vec = ut(as.matrix(dist(food_vars[,1])))
+PC2_vec = ut(as.matrix(dist(food_vars[,2])))
+
+cluster_vec = ut(read.csv("cluster_rdm.csv",row.names = 1))
+category_vec = ut(read.csv("category_rdm.csv",row.names = 1))
+
+subj_data = read.csv(sprintf("%s/RSA_subject_data_%s_zscores.csv",out_dir,task),row.names = 1,check.names = F)
+subjects = as.character(levels(subj_data$sublabel)); rois = as.character(levels(subj_data$roi))
+## ========================================================================================== ##
+
+
